54bcdf474f39e70f7e3bef3331d557b12f455e0a
Daria Shpak <darya.shpak@gmail.com>
2023-06-21 22:55:29 +0300

    Adds task 1 to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import Profile from './Profile/profile.js';
+import user from '../user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+<Profile
+avatar={user.avatar}
+tag={user.tag}
+username={user.username}
+location={user.location}
+stats={user.stats}
+/>
     </div>
-  );
+);
 };

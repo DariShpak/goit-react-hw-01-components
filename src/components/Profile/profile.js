3e838ab744b0179f1cb66e02884c07ac930ac22d
Daria Shpak <darya.shpak@gmail.com>
2023-06-22 12:59:39 +0300

    Adds .isRequired param to PropTypes

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -3,40 +3,37 @@ import PropTypes from 'prop-types'
 function Profile({ avatar, tag, username, location, stats}) {
   return (
     <div>
-
     <div>
-      
     <img src={avatar} alt={tag} width="200"/>
-<h2>{username}</h2>
-<p>@{tag}</p>
-<p>{location}</p>
-</div>
-<ul>
-<li>
-  <span>Followers</span>
-  <span>{stats.followers}</span>
+    <h2>{username}</h2>
+    <p>@{tag}</p>
+    <p>{location}</p>
+    </div>
+    <ul>
+    <li>
+    <span>Followers</span>
+    <span>{stats.followers}</span>
  
-</li>
-<li>
-  <span>Views</span>
-  <span>{stats.views}</span>
+    </li>
+    <li>
+     <span>Views</span>
+     <span>{stats.views}</span>
 
-  </li>
-  <li>
-  <span>Likes</span>
-  <span>{stats.likes}</span>
-
-  </li>
-</ul>
-</div>);
+    </li>
+    <li>
+    <span>Likes</span>
+    <span>{stats.likes}</span>
+    </li>
+    </ul>
+    </div>);
 };
 
 Profile.propTypes = {
-avatar: PropTypes.string,
-tag: PropTypes.string,
-username: PropTypes.string,
-location: PropTypes.string,
-stats: PropTypes.object,
+avatar: PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired,
+username: PropTypes.string.isRequired,
+location: PropTypes.string.isRequired,
+stats: PropTypes.object.isRequired,
 
 }
 

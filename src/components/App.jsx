2b2ee40fc02e968e73fc6d01590bb306bbceef73
Daria Shpak <darya.shpak@gmail.com>
2023-06-23 17:19:32 +0300

    Adds task 2 components to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
 import { Profile} from 'components/Profile/profile';
-import user from 'user.json';
-
+import user from 'data/user.json';
+// import { StatisticsTitle } from 'components/Statistics/StatTitle/StatisticsTitle'
+import data from 'data/data.json'
+import { StatList } from 'components/Statistics/StatList'
 
 export const App = () => {
   return (
     <div>
-<Profile
+{<Profile
 avatar={user.avatar}
 tag={user.tag}
 username={user.username}
 location={user.location}
 stats={user.stats}
-/>
+/>}
+{<StatList title="Upload stats" stats={data} />}
     </div>
     )
 }

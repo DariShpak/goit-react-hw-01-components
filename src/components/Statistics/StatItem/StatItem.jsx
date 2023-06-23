cbdd9684fab8470d408c1629043e6311da95a6bb
Daria Shpak <darya.shpak@gmail.com>
2023-06-24 01:15:29 +0300

    Updates color pallets of task 2, 4

--- src/components/Statistics/StatItem/StatItem.jsx
+++ src/components/Statistics/StatItem/StatItem.jsx
@@ -11,7 +11,7 @@ export const StatItem = ({ label, percentage}) => {
 }
 
 function getRandomColor() {
-  const colorPalette = ["#667b68", "#dde6d5","#b88c8c", "#ddadad", "#ddadad", "#ddadad", "#8caba8", "#aec8ce", "#dfdfde", "#a2798f", ];
+  const colorPalette = ["#667b68", "#5f9ea0", "#a3c1ad", "#a0d6b4","#fceee9", "#dde6d5","#b88c8c", "#ddadad", "#8caba8", "#aec8ce", "#dfdfde", "#a2798f", ];
   const randomColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
   return randomColor;
 }
